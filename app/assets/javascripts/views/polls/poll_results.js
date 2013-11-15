29dc2a98b3962d23fc3d132e1dc032b2494f8d7e
Jeff Rosen <jeffrey.s.rosen@gmail.com>
2013-11-14 17:21:37 -0800

    Now you can refresh colors on one graph at a time

--- app/assets/javascripts/views/polls/poll_results.js
+++ app/assets/javascripts/views/polls/poll_results.js
@@ -5,11 +5,11 @@ PollCheetah.Views.PollResults = Backbone.View.extend({
     var that = this;
     that.listenTo(that.model, 'change', that.render.bind(that));
     that.model.refreshResults();
-    that.colorArray = [];
+    that.colorArrays = {};
   },
 
   events: {
-    "click #newColors" : "newColors"
+    "click .newColors" : "newColors"
   },
 
   render: function() {
@@ -20,69 +20,71 @@ PollCheetah.Views.PollResults = Backbone.View.extend({
 
     this.$el.html(renderedContent);
 
-    this.createGraph();
-    this.addColorLabels();
+    this.createGraphs();
     return this;
   },
 
-  createGraph: function(forceNewColors) {
+  createGraphs: function() {
     var that = this;
-    var data = [];
-    this.model._pollQuestions.models.forEach( function(question, index) {
-      data[index] = []
-      
-      if (!this.colorArray || this.colorArray.length === 0 || forceNewColors === true) {
-        var numAnswers = question._questionAnswers.models.length
-        this.colorArray = []
-        var startHue = Math.floor(Math.random()*360);
-        var stepSize = Math.floor(Math.random()*3)
-
-        for(var i = 0; i < numAnswers; i++) {
-          var step = parseInt(360 / (numAnswers + stepSize))
-          var hue = (startHue + i * step) % 360
-          var saturation = Math.floor(75 + Math.random()*25)
-          var lightness = Math.floor(30 + Math.random()*40)
-          this.colorArray.push("hsl(" + hue + "," + saturation + "%," + lightness + "%)")
-        }
-        this.colorArray = _.shuffle(this.colorArray)        
-      }
-
-      question._questionAnswers.models.forEach( function(answer, answerIndex) {
-        data[index].push({
-          value: answer.attributes.num_votes,
-          color: this.colorArray[answerIndex]
-        });
-        answer.color = this.colorArray[answerIndex];
-      });
-      var options = {animationEasing: "easeOutQuint"}
-      var $pieGraph = $('<canvas id="pie' + question.id + '" width="250" height="250" style="width:250px;height:250px;"></canvas>');
-      var $div = that.$("#pie_graph_" + question.id)
-      $div.html($pieGraph);
-      var ctx = that.$("#pie" + question.id).get(0).getContext("2d");
-      var graph = new Chart(ctx).Pie(data[index],options);
+    that.model._pollQuestions.models.forEach( function(question, index) {
+      that.createGraph(question, true);
+      that.addColorLabels(question);
     });
   },
 
-  addColorLabels: function() {
+  createGraph: function(question, forceNewColors) {
+    var data = [];
     var that = this;
-    that.model._pollQuestions.models.reverse().forEach( function(question) {
-      question._questionAnswers.models.reverse().forEach( function(answer) {
-        that.$("#answerLabel" + answer.attributes.id).css("background-color", answer.color);
-      })
+    
+    // create an array of colors, no two of which are too similar
+    if (!this.colorArrays[question.id] || this.colorArrays[question.id].length === 0 || forceNewColors) {
+      var numAnswers = question._questionAnswers.models.length
+      this.colorArrays[question.id] = []
+      var startHue = Math.floor(Math.random()*360);
+      var stepSize = Math.floor(Math.random()*3)
+
+      for(var i = 0; i < numAnswers; i++) {
+        var step = parseInt(360 / (numAnswers + stepSize))
+        var hue = (startHue + i * step) % 360
+        var saturation = Math.floor(75 + Math.random()*25)
+        var lightness = Math.floor(30 + Math.random()*40)
+        this.colorArrays[question.id].push("hsl(" + hue + "," + saturation + "%," + lightness + "%)")
+      }
+      this.colorArrays[question.id] = _.shuffle(this.colorArrays[question.id]);
+    }
+
+    // store the votes and colors according to the Chart.js requirements
+    question._questionAnswers.models.forEach( function(answer, answerIndex) {
+      data.push({
+        value: answer.attributes.num_votes,
+        color: that.colorArrays[question.id][answerIndex]
+      });
+      answer.color = that.colorArrays[question.id][answerIndex]; // this line may not work - need to check
+    });
+
+    // draw the graph
+    var options = {animationEasing: "easeOutQuint"}
+    var $pieGraph = $('<canvas id="pie' + question.id + '" width="250" height="250" style="width:250px;height:250px;"></canvas>');
+    var $div = that.$("#pie_graph_" + question.id)
+    $div.html($pieGraph);
+    var ctx = that.$("#pie" + question.id).get(0).getContext("2d");
+    var graph = new Chart(ctx).Pie(data,options);
+  },
+
+  addColorLabels: function(question) {
+    var that = this;
+    question._questionAnswers.models.reverse().forEach( function(answer) {
+      that.$("#answerLabel" + answer.attributes.id).css("background-color", answer.color);
     })
   },
 
   newColors: function(event) {
     event.preventDefault();
-    this.createGraph(true);
-    this.addColorLabels();
-  },
-
-  // randomPoll: function(event) {
-  //   event.preventDefault();
-  //   var randID = Math.ceil(Math.random() * 20);
-  //   Backbone.history.navigate("/polls/" + randID, { trigger: true });
-  // }
+    var questionID = $(event.target).data("id");
+    question = _.filter(this.model._pollQuestions.models, function(question){ return question.id === questionID; })[0];
+    this.createGraph(question, true);
+    this.addColorLabels(question);
+  }
 });
 
 
